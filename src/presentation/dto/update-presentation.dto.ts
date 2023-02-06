eb806ddce7ea1a958f44179200aaebacb921d389
Wyllwyll <terry.boeuf@hotmail.fr>
2023-02-06 13:39:05 +0100

    add:presentation service debut

--- src/presentation/dto/update-presentation.dto.ts
+++ src/presentation/dto/update-presentation.dto.ts
@@ -1,4 +1,42 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 import { CreatePresentationDto } from './create-presentation.dto';
 
-export class UpdatePresentationDto extends PartialType(CreatePresentationDto) {}
+export class UpdatePresentationDto extends PartialType(CreatePresentationDto) {
+
+    @IsNotEmpty()
+    @IsString()
+    @IsOptional()
+    langue: string
+
+
+    @IsNotEmpty()
+    @IsString()
+    @IsOptional()
+    presentation: string
+
+
+    @IsNotEmpty()
+    @IsString()
+    @IsOptional()
+    nationnalitée: string
+
+
+    @IsOptional()
+    @IsString()
+    genre: string
+
+    @IsOptional()
+    @IsNumber()
+    age: number
+
+
+    @IsOptional()
+    @IsString()
+    hobbies: string
+
+
+
+
+
+}
