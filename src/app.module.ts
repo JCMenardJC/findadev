8ba3428775d8faa7cbc19a32e5f697c3e3471425
GARITEY Jean Sebastien (SNCF RESEAU / INFRAPOLE AQUITAINE / UP4 CSA) <7604674a@commun.ad.sncf.fr>
2023-02-06 11:14:14 +0100

    merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,20 +5,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: 5432,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: [],
-    synchronize: true,
-  }),
-  ConfigModule.forRoot()
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    ConfigModule.forRoot(),
   ],
 
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
