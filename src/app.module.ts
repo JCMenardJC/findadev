0c748b3e839f58e4fb3203351d4fcdac1508cd84
WAM <eraserw@gmail.com>
2023-02-06 13:17:37 +0100

    feat all Langage

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { LangagesModule } from './langages/langages.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: 5432,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: [],
-    synchronize: true,
-  }),
-  ConfigModule.forRoot()
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ['dist/**/*.entity{ .ts,.js}'],
+      synchronize: true,
+    }),
+    ConfigModule.forRoot(),
+    LangagesModule,
   ],
 
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/langages/langages.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@nestjs/common';
+import { CreateLangageDto } from './dto/create-langage.dto';
+import { UpdateLangageDto } from './dto/update-langage.dto';
+import { Langage } from './entities/langage.entity';
+
+@Injectable()
+export class LangagesService {
+  async create(
+    createLangageDto: CreateLangageDto,
+    user_id: number,
+  ): Promise<Langage | undefined> {
+    const newLangage = new Langage();
+    newLangage.user = user_id;
+    let i = 1;
+    while (createLangageDto[`langage_${i}`]) {
+      newLangage[`langage_${i}`] = createLangageDto[`langage_${i}`];
+      i++;
+    }
+    await Langage.save(newLangage);
+
+    return undefined;
+  }
+
+  async findAll(): Promise<Langage[] | undefined> {
+    const data = await Langage.find({
+      select: {
+        user: false,
+      },
+    });
+    if (data[0]) {
+      return data;
+    }
+    return undefined;
+  }
+
+  async findOne(user: number): Promise<Langage | undefined> {
+    const dataUser = await Langage.findOneBy({ user });
+    if (dataUser) {
+      return dataUser;
+    }
+    return undefined;
+  }
+
+  async update(user: number, updateLangageDto: UpdateLangageDto) {
+    const dataUpdated = await Langage.update(user, updateLangageDto);
+
+    if (dataUpdated) {
+      return dataUpdated;
+    }
+    return undefined;
+  }
+
+  async remove(user: number): Promise<Langage | undefined> {
+    const save = await Langage.findOneBy({ user });
+    await Langage.delete({ user });
+    const verif = await Langage.findOneBy({ user });
+    if (verif) {
+      return undefined;
+    }
+    return save;
+  }
+}
