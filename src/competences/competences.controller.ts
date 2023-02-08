d1f0732cb2d9d87cd2154ec8207544b630f3d983
Titou <menardjc.33@gmail.com>
2023-02-08 09:22:40 +0100

    merge le 08/02/2023 a 9h22

--- src/competences/competences.controller.ts
+++ src/competences/competences.controller.ts
@@ -9,16 +9,16 @@ import {
   Res,
   UseGuards,
   Request,
-} from '@nestjs/common';
-import {} from '@nestjs/common/decorators';
-import { Response } from 'express';
-import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { CompetencesService } from './competences.service';
-import { CreateCompetenceDto } from './dto/create-competence.dto';
-import { UpdateCompetenceDto } from './dto/update-competence.dto';
-import { Competence } from './entities/competence.entity';
+} from "@nestjs/common";
+import {} from "@nestjs/common/decorators";
+import { Response } from "express";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { CompetencesService } from "./competences.service";
+import { CreateCompetenceDto } from "./dto/create-competence.dto";
+import { UpdateCompetenceDto } from "./dto/update-competence.dto";
+import { Competence } from "./entities/competence.entity";
 
-@Controller('competences')
+@Controller("competences")
 export class CompetencesController {
   constructor(private readonly competencesService: CompetencesService) {}
 
@@ -27,24 +27,24 @@ export class CompetencesController {
   async create(
     @Body() createCompetenceDto: CreateCompetenceDto,
     @Res() res: Response,
-    @Request() req,
+    @Request() req
   ) {
     const verifUser = await this.findOneById(req.user.user_id);
     console.log(verifUser);
 
     if (verifUser) {
       res.status(401).json({
-        status: '401',
-        message: 'This user has already post his competences !!',
+        status: "401",
+        message: "This user has already post his competences !!",
       });
     } else {
       await this.competencesService.create(
         createCompetenceDto,
-        req.user.user_id,
+        req.user.user_id
       );
       res.status(201).json({
-        status: '201',
-        message: 'Success',
+        status: "201",
+        message: "Success",
         data: createCompetenceDto,
       });
     }
@@ -55,21 +55,21 @@ export class CompetencesController {
     return this.competencesService.findAll();
   }
 
-  @Get(':id')
-  findOneById(@Param('id') id: string) {
+  @Get(":id")
+  findOneById(@Param("id") id: string) {
     return this.competencesService.findOneById(+id);
   }
 
-  @Patch(':id')
+  @Patch(":id")
   update(
-    @Param('id') id: string,
-    @Body() updateCompetenceDto: UpdateCompetenceDto,
+    @Param("id") id: string,
+    @Body() updateCompetenceDto: UpdateCompetenceDto
   ) {
     return this.competencesService.update(+id, updateCompetenceDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.competencesService.remove(+id);
   }
 }
