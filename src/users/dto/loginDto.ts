a6063ae21449b4ba282ae8d416eebc13ac291ae5
Titou <menardjc.33@gmail.com>
2023-02-10 16:38:08 +0100

    commit avany merge

--- src/users/dto/loginDto.ts
+++ src/users/dto/loginDto.ts
@@ -1,16 +1,14 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
-import { Column } from 'typeorm';
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+import { Column } from "typeorm";
 
 export class LoginDto {
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsString()
-    @Column()
-    username: string;
-    @ApiProperty()
-    @IsNotEmpty()
-    @IsString()
-    @Column()
-    password: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  password: string;
 }
