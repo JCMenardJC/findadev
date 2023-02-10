00a3c686f751c2eb31a50d088e4b901e74bab798
GARITEY Jean Sebastien (SNCF RESEAU / INFRAPOLE AQUITAINE / UP4 CSA) <7604674a@commun.ad.sncf.fr>
2023-02-10 15:17:15 +0100

    creation du dto searchlangage

--- src/users/dto/loginDto.ts
+++ src/users/dto/loginDto.ts
@@ -1,16 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
-import { Column } from 'typeorm';
 
 export class LoginDto {
     @ApiProperty()
     @IsNotEmpty()
     @IsString()
-    @Column()
     username: string;
     @ApiProperty()
     @IsNotEmpty()
     @IsString()
-    @Column()
     password: string;
 }
